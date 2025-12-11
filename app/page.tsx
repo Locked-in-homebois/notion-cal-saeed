a4a0c01882f7b6b6085dab69be26b6c252aa096f
saeed-afr <156814585+saeed-afr@users.noreply.github.com>
2025-12-11 23:00:27 +0300

    next.js setup

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
         <p className="p-4 text-center text-xl max-w-lg">
           All of your commitments, now in one place. Meet the beautifully designed, fully integrated calendar for your work and life.
         </p>
-        <Link className="mb-8" href="/">
+        <Link className="md:mb-8" href="/">
         <button className="bg-black text-white font-semibold px-4 py-2 rounded-md hover:bg-gray-800 transition">
           Get Notion Calendar free
         </button>
@@ -31,6 +31,32 @@ export default function HomePage() {
           height={600}
           draggable="false"
         />
+        <h1 className="text-4xl font-semibold justify-center text-center mt-16">
+          Time management simplified.
+        </h1>
+
+        <div className="container w-270 bg-[#f6f5f4] rounded-lg shadow-2xl mt-16 ">
+          <Image className="m-6"
+            src="flash-outline.svg"
+            alt="Flash icon"
+            width={30}
+            height={30}
+            draggable="false"
+          />
+          <h3 className="p-6 font-extrabold text-2xl mb-0">
+            See Your schedule at a Glance        
+          </h3>
+          <p className="p-6 text-md max-w-lg mb-0">
+          Join meetings directly from the menu bar so you can stay focused on the work that matters.
+          </p>
+          <Image className="rounded-b-lg"
+          src="/menu-bar.avif"
+          alt="Notion Cal menu bar picture"
+          width={1500}
+          height={600}
+          draggable="false"
+          />
+        </div>
     </main>
   );
 }
